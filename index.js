300e7efced227838d77ea3a98125e629e6757e92
Raul Gallo Dagir <rauldagir@hotmail.com>
2018-07-19 11:18:52 -0700

    Changing server config

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'https://test-livequery.herokuapp.com/parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'testlive',
+  masterKey: process.env.MASTER_KEY || 'testlivemasterkey', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://test-livequery.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["Message", "Conversation"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
