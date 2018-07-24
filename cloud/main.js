5a1eec32a7bb3d68fd713b12383ff33f1abd2e01
rlxu <xu.ruili99@gmail.com>
2018-07-24 11:14:45 -0700

    Add query to allow push to specific user

--- cloud/main.js
+++ cloud/main.js
@@ -5,18 +5,21 @@ Parse.Cloud.define('pushNotificationGeneral', function(request, response) {
   var user = request.user;
 
   var newData = params.newData;
+  var receiver = params.receiver;
 
   // set audience of push notification and data
   var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
+  if (receiver) {
+    pushQuery.equalTo("currentUserId", receiver);
+  } else {
+    pushQuery.equalTo("deviceType", "android");
+  }
 
   var payload = {};
 
   if (newData) {
       payload.mydata = newData;
   }
-
-  // Note that useMasterKey is necessary for Push notifications to succeed.
 
   Parse.Push.send({
   where: pushQuery,      // for sending to a specific channel
